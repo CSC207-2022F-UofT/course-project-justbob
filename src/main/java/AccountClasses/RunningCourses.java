5737de6c3721288e34d829163d1188b291bd3b97
AlanC12138 <alanchen0705@gmail.com>
2022-10-31 13:38:13 -0400

    added add and remove course functions to RunningCourses.java

--- src/main/java/AccountClasses/RunningCourses.java
+++ src/main/java/AccountClasses/RunningCourses.java
@@ -1,12 +1,20 @@
 package AccountClasses;
 
+import java.util.ArrayList;
+
 public class RunningCourses {
-    List<Course> runningCourses;
+    List<Course> runningCourses = new ArrayList<Course>();
     Calendar calendar;
     Checklist checklist;
     GPATrendOverall semesterGPA;
 
     public RunningCourses(){}
 
+    public void addRunningCourse(Course course){
+        runningCourses.add(course);
+    }
+    public void removeRunningCourse(Course course){
+        runningCourses.remove(course);
+    }
 }
 
