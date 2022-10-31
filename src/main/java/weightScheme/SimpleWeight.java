2dad8a9e2736150f0df7145f910ff349118a7deb
Sunshine <armonjam@gmail.com>
2022-10-31 12:57:02 -0400

    Added simpleweight class and fixed return type

---
+++ src/main/java/weightScheme/SimpleWeight.java
@@ -0,0 +1,20 @@
+package weightScheme;
+
+import java.util.List;
+
+public class SimpleWeight implements weightScheme {
+    @Override
+    public float computeWeight(List<Float> marks) {
+        return 0;
+    }
+
+    @Override
+    public float getTotalWeight() {
+        return 0;
+    }
+
+    @Override
+    public int getNumberOfInstances() {
+        return 0;
+    }
+}
