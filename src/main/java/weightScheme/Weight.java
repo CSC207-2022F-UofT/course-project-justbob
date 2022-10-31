19dfb9a8ae6506ebe9a26e434d12520567229c99
Sunshine <armonjam@gmail.com>
2022-10-31 12:47:43 -0400

    Added constructor

--- src/main/java/weightScheme/Weight.java
+++ src/main/java/weightScheme/Weight.java
@@ -4,6 +4,11 @@ public class Weight {
     private int numberOfInstances;
     private float weightOfEachInstance;
 
+    public Weight(int numberOfInstances, float weightOfEachInstance) {
+        this.numberOfInstances = numberOfInstances;
+        this.weightOfEachInstance = weightOfEachInstance;
+    }
+
     public float getNumberOfInstances() {
         return numberOfInstances;
     }
