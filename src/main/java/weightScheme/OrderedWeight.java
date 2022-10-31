35b512396101dd34d5105c59e1272adf2380430d
Sunshine <armonjam@gmail.com>
2022-10-31 19:43:00 -0400

    Added weightscheme interface overrides

--- src/main/java/weightScheme/OrderedWeight.java
+++ src/main/java/weightScheme/OrderedWeight.java
@@ -1,4 +1,20 @@
 package weightScheme;
 
-public class OrderedWeight {
+public class OrderedWeight implements WeightScheme {
+    private Weight[] orderedWeights;
+
+    @Override
+    public double computeWeight(double[] marks) {
+        return 0;
+    }
+
+    @Override
+    public int getNumberOfInstances() {
+        return 0;
+    }
+
+    @Override
+    public double getTotalWeight() {
+        return 0;
+    }
 }
