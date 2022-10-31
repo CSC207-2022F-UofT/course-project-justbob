0e753343e4d545ea580c18397859ba9a418c8df0
Sunshine <armonjam@gmail.com>
2022-10-31 13:55:29 -0400

    Added tests

--- src/test/java/weightScheme/WeightSchemeTest.java
+++ src/test/java/weightScheme/WeightSchemeTest.java
@@ -1,2 +1,15 @@
-package weightScheme;public class WeightSchemeTest {
+package weightScheme;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+class WeightSchemeTest {
+    private double[] marks = {100, 95, 80, 75.2};
+
+
+    @Test
+    public void SimpleWeightComputation() {
+    }
 }
