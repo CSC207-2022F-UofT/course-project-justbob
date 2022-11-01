06764f07e376271fe3a62b174aa25e941fdde2c2
Sunshine <armonjam@gmail.com>
2022-11-01 16:36:30 -0400

    Added constructor doc

--- src/main/java/weightScheme/Weight.java
+++ src/main/java/weightScheme/Weight.java
@@ -4,6 +4,11 @@ public class Weight {
     private int numberOfInstances;
     private double weightOfEachInstance;
 
+    /**
+     * Create a new Weight object
+     * @param numberOfInstances must be positive
+     * @param weightOfEachInstance must be between 0 and 1
+     */
     public Weight(int numberOfInstances, double weightOfEachInstance) {
         this.numberOfInstances = numberOfInstances;
         this.weightOfEachInstance = weightOfEachInstance;
