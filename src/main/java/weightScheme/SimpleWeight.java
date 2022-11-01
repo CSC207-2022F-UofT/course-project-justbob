cf4a28f1752cf6f3d2df29a12244166fd8f2556a
Sunshine <armonjam@gmail.com>
2022-11-01 16:38:32 -0400

    Removed low-level simpleWeight constructor

--- src/main/java/weightScheme/SimpleWeight.java
+++ src/main/java/weightScheme/SimpleWeight.java
@@ -9,10 +9,6 @@ public class SimpleWeight implements WeightScheme {
         this.weight = weight;
     }
 
-    public SimpleWeight(int numberOfInstances, double weightOfEachInstance) {
-        this.weight = new Weight(numberOfInstances, weightOfEachInstance);
-    }
-
     @Override
     public double computeWeighted(double[] marks) {
         return DoubleStream.of(marks)
