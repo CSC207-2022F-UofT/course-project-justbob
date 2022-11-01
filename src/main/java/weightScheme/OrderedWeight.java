4a62f7a3b347c48a0b6da53c978921095cf6c1c2
Sunshine <armonjam@gmail.com>
2022-10-31 20:15:37 -0400

    Implemented compute weight using pipeline

--- src/main/java/weightScheme/OrderedWeight.java
+++ src/main/java/weightScheme/OrderedWeight.java
@@ -1,13 +1,26 @@
 package weightScheme;
 
+import java.util.Arrays;
 import java.util.stream.Stream;
+import java.util.stream.DoubleStream;
 
 public class OrderedWeight implements WeightScheme {
     private Weight[] orderedWeights;
 
     @Override
     public double computeWeight(double[] marks) {
-        return 0;
+        double result = 0;
+        int current_index = 0;
+        for (Weight weight : orderedWeights) {
+            result += DoubleStream.of(marks)
+                    .sorted()
+                    .skip(current_index)
+                    .limit(weight.getNumberOfInstances())
+                    .map(mark -> mark * weight.getWeightOfEachInstance())
+                    .sum();
+            current_index += weight.getNumberOfInstances();
+        }
+        return result;
     }
 
     @Override
