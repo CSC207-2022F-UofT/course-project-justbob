e4cfb6e0115487d8084a4baec07adb39fe98cba9
AlanC12138 <alanchen0705@gmail.com>
2022-11-14 13:22:42 -0500

    Added login interactor

--- src/main/java/AccountClasses/AccountInteractor.java
+++ src/main/java/AccountClasses/AccountInteractor.java
@@ -2,16 +2,17 @@ package AccountClasses;
 
 import java.util.*;
 
-public class AccountInteractor{
+public class AccountInteractor {
     public Scanner scanner;
 
-    public AccountInteractor(){
+    public AccountInteractor() {
         scanner = new Scanner(System.in);
     }
-    public void createAccount(){
+
+    public void createAccount() {
         String id = idInput();
-        for(Account account : AccountList.accounts){
-            if(Objects.equals(account.username, id)){
+        for (Account account : AccountList.accounts) {
+            if (Objects.equals(account.username, id)) {
                 System.out.println("Username already exists, please choose another username");
                 createAccount();
                 return;
@@ -21,21 +22,41 @@ public class AccountInteractor{
         Account newAcc = new Account(id, password);
         AccountList.addAccount(newAcc);
         System.out.println("Account has been successfully created");
-
     }
 
-    private String idInput(){
+    public void login() {
+        String username = idInput();
+        String password = passwordInput();
+        for (Account account : AccountList.accounts) {
+            if (Objects.equals(account.username, username)) {
+                if (Objects.equals(account.password, password)) {
+                    closeScanner();
+                    System.out.println("Login is successful");
+                    return;
+                }else{
+                    System.out.println("Incorrect password");
+                    login();
+                }
+            }else{
+                System.out.println("Username does not exist");
+                login();
+            }
+        }
+    }
+
+    private String idInput() {
         System.out.println("Enter Username");
         return scanner.nextLine();
     }
-    private String passwordInput(){
+
+    private String passwordInput() {
         System.out.println("Enter Password (8-16 characters, alphabets and numbers only)");
         return scanner.nextLine();
     }
 
-    private String passwordCheck(){
+    private String passwordCheck() {
         String password = passwordInput();
-        if(!(8 <= password.length() && password.length() <= 16)){
+        if (!(8 <= password.length() && password.length() <= 16)) {
             System.out.println("Invalid password,password must be between 8 to 16 characters");
             passwordCheck();
         }
@@ -43,10 +64,11 @@ public class AccountInteractor{
         return password;
     }
 
-    public void closeScanner(){
+    public void closeScanner() {
         scanner.close();
     }
-    public void removeAccount(Account account){
+
+    public void removeAccount(Account account) {
         AccountList.removeAccount(account);
     }
 }
