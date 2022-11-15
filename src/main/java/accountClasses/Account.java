2186639b291931653d5b47577784125d9c36d120
AlanC12138 <alanchen0705@gmail.com>
2022-11-15 14:16:56 -0500

    wrote documentation for Account finalized Account commented RunningCourses

--- src/main/java/accountClasses/Account.java
+++ src/main/java/accountClasses/Account.java
@@ -2,13 +2,22 @@ package accountClasses;
 
 public class Account {
     public String username;
+    public String password;
     public RunningCourses runningCourses;
     public Archive archive;
-    public String password;
 
+    /**
+     * Creates an account object
+     * @param id the username of the account
+     *           e.g. 'JimBob123'
+     * @param password the password of the account
+     *           e.g. 'JimmyJimmy321'
+     */
     public Account(String id, String password) {
         this.username = id;
         this.password = password;
+        this.runningCourses = new RunningCourses();
+        this.archive = new Archive();
     }
 }
 
--- src/main/java/accountClasses/RunningCourses.java
+++ src/main/java/accountClasses/RunningCourses.java
@@ -1,27 +1,38 @@
 package accountClasses;
 
+import courseManager.Course;
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class RunningCourses {
-    List<Course> runningCourses = new ArrayList<Course>();
+    List<Course> semester;
     Calendar calendar;
     Checklist checklist;
     CGPA semesterGPA;
 
     public RunningCourses(){
+        this.semester = new ArrayList<Course>();
+        this.calendar
+        this.checklist
     }
 
     public void addRunningCourse(Course course){
-        runningCourses.add(course);
+        //Add new course to the current semster
+        semester.add(course);
     }
     public void removeRunningCourse(Course course){
-        runningCourses.remove(course);
+        semester.remove(course);
     }
-    public List<Double> getGradesList(){
 
-    }
     public CGPA getOverallGpa(){
-        return /**call CGPA calculator**/
+        //Show the OverallGpa for the current semester
+        return semesterGPA;
+    }
+
+    public void updateOverallGpa(){
+        //Calculate the current Gpa for the semester
+        //call gpa calculator
     }
 }
 
