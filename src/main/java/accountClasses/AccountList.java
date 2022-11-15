1fcdf110ce2249708ba5f747e5306a914f9e0876
AlanC12138 <alanchen0705@gmail.com>
2022-11-15 14:04:05 -0500

    fixed keywords for accounts

--- src/main/java/accountClasses/AccountList.java
+++ src/main/java/accountClasses/AccountList.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AccountList {
-    static List<Account> accounts; //The list of registered accounts.
+    static List<Account> accounts = new ArrayList<Account>(); //The list of registered accounts.
 
     static public void addAccount(Account account) {
         //This method adds a new account to accounts
