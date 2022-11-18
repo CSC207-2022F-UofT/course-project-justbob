d3a0093b845c5277f3b3db0b2dc7d56462020efe
Deep Jain <deepsjain123@gmail.com>
2022-11-18 14:29:25 -0500

    gradeTrendView updates given frame

--- src/main/java/gradeTrend/gradeTrendView.java
+++ src/main/java/gradeTrend/gradeTrendView.java
@@ -2,31 +2,30 @@ package gradeTrend;
 
 import org.knowm.xchart.CategoryChart;
 import org.knowm.xchart.SwingWrapper;
+import org.knowm.xchart.XChartPanel;
 
-import java.util.ArrayList;
+import javax.swing.*;
 import java.util.List;
 
 public class gradeTrendView {
-    public static void showGraph(String name, List<String> xData, List<Double> yData) {
+    private final JFrame frame;
+
+    public gradeTrendView(JFrame frame) {
+        this.frame = frame;
+    }
+
+    public void updateFrame(gradeTrendInterface t) {
+        String name = t.toString();
+        List<String> xData = t.getXData();
+        List<Double> yData = t.getYData();
+
         CategoryChart chart = new CategoryChart(1000, 500);
         chart.addSeries(name, xData, yData);
 
-        // XChartPanel<CategoryChart> chartPanel = new XChartPanel<>(chart);
-        SwingWrapper<CategoryChart> sw = new SwingWrapper<>(chart);
-        sw.displayChart();
-    }
+        JPanel chartPanel = new XChartPanel<>(chart);
 
-    public static void main(String[] args) {
-        List<String> xData = new ArrayList<>();
-        List<Double> yData = new ArrayList<>();
-        xData.add("Course 1");
-        xData.add("Course 2");
-        xData.add("Course 3");
-        yData.add(1.0);
-        yData.add(1.5);
-        yData.add(2.75);
-
-        gradeTrendView.showGraph("Example Graph", xData, yData);
+        this.frame.removeAll();
+        this.frame.add(chartPanel);
     }
 
     public void showGraph(gradeTrendInterface t) {
@@ -36,6 +35,8 @@ public class gradeTrendView {
 
         CategoryChart chart = new CategoryChart(1000, 500);
         chart.addSeries(name, xData, yData);
+
+        JPanel chartPanel = new XChartPanel<>(chart);
 
         SwingWrapper<CategoryChart> sw = new SwingWrapper<>(chart);
         sw.displayChart();
