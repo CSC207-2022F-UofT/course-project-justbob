4e1f32b82df347f28e7b823352da800b05d73d45
Vaishnav <vaishnav.kuhiraman@mail.utoronto.ca>
2022-11-19 17:37:36 -0500

    archievemarks list edits

--- src/main/java/Calculations/CGPA.java
+++ src/main/java/Calculations/CGPA.java
@@ -3,13 +3,19 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class CGPA extends mathGpaCgpa {
-    private Archieve archivepast;
+
 
     public CGPA(double percentage) {
         super(percentage);
     }
-    public double CGPAofArchieve(){
-        archivepast = new Archieve();
+    public List<Double> archivemarks(){
+        List<Double> archivemarks = new ArrayList<>();
+        List<Course> pastcourses = Archieve.getArchieved();
+        for (int i = 0; i < pastcourses.size(); i++){
+            archivemarks.add(pastcourses);
+        }
+
+
 
 
     }
