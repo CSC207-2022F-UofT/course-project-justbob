6274bb9f1b9d144325ee8690f1339acab75a9d57
Vaishnav <vaishnav.kuhiraman@mail.utoronto.ca>
2022-11-20 16:34:55 -0500

    converts list of percentage marks to lettered grades.

--- src/main/java/Calculations/computeGradeLetter.java
+++ src/main/java/Calculations/computeGradeLetter.java
@@ -1,43 +1,43 @@
 package Calculations;
 
 public class computeGradeLetter {
-    private double percentage;
+    private double percentage1;
 
-    public computeGradeLetter(double percentage){
-        this.percentage = percentage;
+    public computeGradeLetter(double percentage1){
+        this.percentage1 = percentage1;
     }
 
     public double getPercentage() {
-        return percentage;
+        return percentage1;
     }
 
-    public void setPercentage(double percentage) {
-        this.percentage = percentage;
+    public void setPercentage(double percentage1) {
+        this.percentage1 = percentage1;
     }
-    public String percentToGrade(){
-        if(percentage >= 90){
+    public static String percentToGrade(double percentage1){
+        if(percentage1 >= 90){
             return "A+";
-        }else if(percentage >= 85){
+        }else if(percentage1 >= 85){
             return "A";
-        }else if(percentage >= 80){
+        }else if(percentage1 >= 80){
             return "A-";
-        }else if(percentage >= 77){
+        }else if(percentage1 >= 77){
             return "B+";
-        }else if(percentage >= 73){
+        }else if(percentage1 >= 73){
             return "B";
-        }else if(percentage >= 70){
+        }else if(percentage1 >= 70){
             return "B-";
-        }else if(percentage >= 67){
+        }else if(percentage1 >= 67){
             return "C+";
-        }else if(percentage >= 63){
+        }else if(percentage1 >= 63){
             return "C";
-        }else if(percentage >= 60){
+        }else if(percentage1 >= 60){
             return "C-";
-        }else if(percentage >= 57){
+        }else if(percentage1 >= 57){
             return "D+";
-        }else if(percentage >= 55){
+        }else if(percentage1 >= 55){
             return "D";
-        }else if(percentage >= 50){
+        }else if(percentage1 >= 50){
             return "D-";
         }else {
             return "F";
