73bece1452303ee0cdc145bfbf6bc636fa5cad5e
MrSunshineVS <armonjam@gmail.com>
2022-11-20 16:16:31 -0500

    Added account response model

---
+++ src/main/java/usecases/dataStorage/AccountDsResponseModel.java
@@ -0,0 +1,31 @@
+package usecases.dataStorage;
+
+public class AccountDsResponseModel {
+    private String username;
+    private String password;
+    private CourseDsResponseModel[] semesterData;
+    private CourseDsResponseModel[] archiveData;
+
+    public AccountDsResponseModel(String username, String password, CourseDsResponseModel[] semesterData, CourseDsResponseModel[] archiveData) {
+        this.username = username;
+        this.password = password;
+        this.semesterData = semesterData;
+        this.archiveData = archiveData;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public CourseDsResponseModel[] getSemesterData() {
+        return semesterData;
+    }
+
+    public CourseDsResponseModel[] getArchiveData() {
+        return archiveData;
+    }
+}
