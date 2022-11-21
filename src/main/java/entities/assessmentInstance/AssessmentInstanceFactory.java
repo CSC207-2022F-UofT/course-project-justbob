233910526af4cd335ce81726491bc9cf5718921e
MrSunshineVS <armonjam@gmail.com>
2022-11-21 01:58:22 -0500

    Modified instance factory to support creating instances from pure data

--- src/main/java/entities/assessmentInstance/AssessmentInstanceFactory.java
+++ src/main/java/entities/assessmentInstance/AssessmentInstanceFactory.java
@@ -1,9 +1,38 @@
 package entities.assessmentInstance;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class AssessmentInstanceFactory implements AssessmentInstanceFactoryInterface{
 
     @Override
     public AssessmentInstance create(String assessmentInstanceName) {
         return new AssessmentInstance(assessmentInstanceName);
+    }
+
+    /**
+     * Create a new assessment instance with all data. Useful for constructing instances from a data storage model.
+     * @param assessmentInstanceName
+     * @param dueDate
+     * @param dueTime
+     * @param mark
+     * @param isCommitted
+     * @param isSubmitted
+     * @return the constructed AssessmentInstance.
+     */
+    @Override
+    public AssessmentInstance create(String assessmentInstanceName, LocalDate dueDate, LocalTime dueTime, Double mark,
+                                        boolean isCommitted, boolean isSubmitted) {
+        AssessmentInstance assessmentInstance = new AssessmentInstance(assessmentInstanceName);
+        assessmentInstance.setDueDate(dueDate);
+        assessmentInstance.setDueTime(dueTime);
+        assessmentInstance.setMark(mark);
+        if (isSubmitted) {
+            assessmentInstance.submit();
+        }
+        if (isCommitted) {
+            assessmentInstance.commit();
+        }
+        return assessmentInstance;
     }
 }
--- src/main/java/entities/assessmentInstance/AssessmentInstanceFactoryInterface.java
+++ src/main/java/entities/assessmentInstance/AssessmentInstanceFactoryInterface.java
@@ -1,5 +1,10 @@
 package entities.assessmentInstance;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public interface AssessmentInstanceFactoryInterface {
     AssessmentInstance create(String assessmentInstanceName);
+    AssessmentInstance create(String assessmentInstanceName, LocalDate dueDate, LocalTime dueTime, Double mark,
+                              boolean isCommitted, boolean isSubmitted);
 }
