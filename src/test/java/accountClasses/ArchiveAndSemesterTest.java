3dd47da59ed37070ad1faefe9d679b808c7b66e1
AlanC12138 <alanchen0705@gmail.com>
2022-11-20 21:45:44 -0500

    removed unwanted code

--- src/test/java/accountClasses/ArchiveAndSemesterTest.java
+++ src/test/java/accountClasses/ArchiveAndSemesterTest.java
@@ -8,11 +8,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ArchiveAndSemesterTest {
-    private final Account goodCommonAccount1 = new Account("Bobby001", "goodbob1998");
-    private final Account duplicateCommonAccount = new Account("Bobby001", "goodbob1997");
-    private final Account badCommonAccount = new Account("Bob", "goodbob1996");
     private final Course course1 = new Course("CSC101", "Intro to CS", "Fall 2021", true, 1.0F);
-    private final Course course2 = new Course("CSC207", "Software Design", "Fall 2022", false, 0.5F);
     private Archive testArchive = new Archive();
     private Semester testSemester = new Semester();
 
