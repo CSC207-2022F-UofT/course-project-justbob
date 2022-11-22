9c30dcdd507cb974d2ec096371943889cc713305
MrSunshineVS <armonjam@gmail.com>
2022-11-21 19:02:34 -0500

    Removed static factory method

--- src/main/java/entities/account/AccountFactory.java
+++ src/main/java/entities/account/AccountFactory.java
@@ -2,7 +2,7 @@ package entities.account;
 
 public class AccountFactory implements AccountFactoryInterface {
 
-    public static Account create(String username, String password) {
+    public Account create(String username, String password) {
         return new Account(username, password);
     }
 }
