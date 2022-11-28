24939c81539f7d7bf4cf879671866e403fccbc9a
YuweiCai2003 <113796739+YuweiCai2003@users.noreply.github.com>
2022-11-28 11:53:14 -0500

    Delete AssessmentClFactory.java

--- src/main/java/AssessmentChecklist/AssessmentClFactory.java
+++
@@ -1,8 +0,0 @@
-package AssessmentChecklist;
-
-public class AssessmentClFactory implements AssessmentClFactoryInterface{
-    @Override
-    public AssessmentCl create(String courseName, String name) {
-        return new AssessmentCl(courseName, name);
-    }
-}
