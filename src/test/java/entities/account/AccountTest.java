f2d2d2253f431c1aea39b8bf657bda31144ce96f
Vaishnav <vaishnav.kuhiraman@mail.utoronto.ca>
2022-11-29 16:03:48 -0500

    writing unit test for method created

---
+++ src/test/java/entities/account/AccountTest.java
@@ -0,0 +1,14 @@
+package entities.account;
+
+import org.junit.jupiter.api.Test;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountTest {
+    public void truePassword(){
+        var pass1 = new Account();
+        assertEquals(True, Account.);
+
+    }
+
+}
