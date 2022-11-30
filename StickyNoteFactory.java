07028f01bbcfe38554fded5e8b6aca12bd26d084
berrythebear <93696493+berrythebear@users.noreply.github.com>
2022-11-30 16:38:50 -0500

    Add files via upload

---
+++ StickyNoteFactory.java
@@ -0,0 +1,5 @@
+package entities.calendarSystem;
+
+public interface StickyNoteFactory {
+    StickyNoteInterface create();
+}
---
+++ StickyNoteInterface.java
@@ -0,0 +1,14 @@
+package entities.calendarSystem;
+
+public interface StickyNoteInterface {
+
+    String getTitle();
+
+    String getType();
+
+    String getLocation();
+
+    String[] getDotJots();
+
+    String[] getDates();
+}
