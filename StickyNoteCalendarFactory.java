0f906363b9ebf5d576f333bbdd6cf76b36a4c00d
berrythebear <93696493+berrythebear@users.noreply.github.com>
2022-11-30 16:45:23 -0500

    Delete StickyNoteCalendarFactory.java

--- StickyNoteCalendarFactory.java
+++
@@ -1,8 +0,0 @@
-package entities.calendarSystem;
-
-public class StickyNoteCalendarFactory implements CalendarFactory {
-    @Override
-    public CalendarInterface create() {
-        return new StickyNoteCalendar();
-    }
-}
