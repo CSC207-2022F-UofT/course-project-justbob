e05fa4f7297560de3ae9431ff2b9d87186d19536
berrythebear <93696493+berrythebear@users.noreply.github.com>
2022-11-30 16:44:10 -0500

    Delete CommonStickyNoteFactory.java

--- CommonStickyNoteFactory.java
+++
@@ -1,8 +0,0 @@
-package entities.calendarSystem;
-
-public class CommonStickyNoteFactory implements StickyNoteFactory {
-    @Override
-    public StickyNoteInterface create(String title, String type, String location, String[] dotJots, String[] dates) {
-        return new CommonStickyNote(title, type, location, dotJots, dates);
-    }
-}
