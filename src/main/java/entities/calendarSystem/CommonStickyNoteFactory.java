f41a8e4a5a7b7855fddb23a7d555041f0ce0ea2d
berrythebear <93696493+berrythebear@users.noreply.github.com>
2022-12-01 09:31:38 -0500

    Update CommonStickyNoteFactory.java

--- src/main/java/entities/calendarSystem/CommonStickyNoteFactory.java
+++ src/main/java/entities/calendarSystem/CommonStickyNoteFactory.java
@@ -1,4 +1,10 @@
 package entities.calendarSystem;
 
-public class CommonStickyNoteFactory {
+import java.time.LocalDate;
+
+public class CommonStickyNoteFactory implements StickyNoteFactory {
+    @Override
+    public StickyNoteInterface create(String title, String type, String location, String[] dotJots, LocalDate[] dates) {
+        return new CommonStickyNote(title, type, location, dotJots, dates);
+    }
 }
