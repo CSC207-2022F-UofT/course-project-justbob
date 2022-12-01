0ec6da62a86cf3970e2f66989135939200e02f48
berrythebear <93696493+berrythebear@users.noreply.github.com>
2022-12-01 09:27:51 -0500

    Update CommonStickyNote.java

--- src/main/java/entities/calendarSystem/CommonStickyNote.java
+++ src/main/java/entities/calendarSystem/CommonStickyNote.java
@@ -1,4 +1,42 @@
 package entities.calendarSystem;
 
-public class CommonStickyNote {
+public class CommonStickyNote implements StickyNoteInterface {
+    private final String title;
+    private final String type;
+    private final String location;
+    private final String[] dotJots;
+    private final String[] dates;
+
+    CommonStickyNote(String title, String type, String location, String[] dotJots, String[] dates) {
+        this.title = title;
+        this.type = type;
+        this.location = location;
+        this.dotJots = dotJots;
+        this.dates = dates;
+    }
+
+    @Override
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String getType() {
+        return type;
+    }
+
+    @Override
+    public String getLocation() {
+        return location;
+    }
+
+    @Override
+    public String[] getDotJots() {
+        return dotJots;
+    }
+
+    @Override
+    public String[] getDates() {
+        return dates;
+    }
 }
