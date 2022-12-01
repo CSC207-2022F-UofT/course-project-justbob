6b6a711c26de972c8872347fdeed9b52047d8869
berrythebear <93696493+berrythebear@users.noreply.github.com>
2022-12-01 11:43:14 -0500

    Update StickyNoteFactory.java

--- src/main/java/entities/calendarSystem/StickyNoteFactory.java
+++ src/main/java/entities/calendarSystem/StickyNoteFactory.java
@@ -3,5 +3,5 @@ package entities.calendarSystem;
 import java.time.*;
 
 public interface StickyNoteFactory {
-    StickyNoteInterface create(String title, String type, String location, String[] dotJots, LocalDate[] dates);
+    StickyNoteInterface create(String title, String type, String location, String time, String[] dotJots, LocalDate[] dates);
 }
