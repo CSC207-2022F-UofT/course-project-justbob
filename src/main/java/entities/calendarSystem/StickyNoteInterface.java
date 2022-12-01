4b20892178f35ed33f46e6b5a922a87b833397ef
berrythebear <93696493+berrythebear@users.noreply.github.com>
2022-12-01 09:30:05 -0500

    Update StickyNoteInterface.java

--- src/main/java/entities/calendarSystem/StickyNoteInterface.java
+++ src/main/java/entities/calendarSystem/StickyNoteInterface.java
@@ -1,4 +1,16 @@
 package entities.calendarSystem;
 
+import java.time.LocalDate;
+
 public interface StickyNoteInterface {
+
+    String getTitle();
+
+    String getType();
+
+    String getLocation();
+
+    String[] getDotJots();
+
+    LocalDate[] getDates();
 }
