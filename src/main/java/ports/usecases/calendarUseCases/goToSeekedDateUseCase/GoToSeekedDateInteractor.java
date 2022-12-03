67ee9f71d308a8161bfb8e32d8f68cf85ba06c92
berrythebear <93696493+berrythebear@users.noreply.github.com>
2022-12-03 00:26:46 -0500

    Delete GoToSeekedDateInteractor.java

--- src/main/java/ports/usecases/calendarUseCases/goToSeekedDateUseCase/GoToSeekedDateInteractor.java
+++
@@ -1,9 +0,0 @@
-package ports.usecases.calendarUseCases.goToSeekedDateUseCase;
-
-public class GoToSeekedDateInteractor implements GoToSeekedDateInputBoundary {
-    @Override
-    public GoToSeekedDateResponseModel execute(GoToSeekedDateRequestModel requestModel) {
-        GoToSeekedDateResponseModel responseModel = new GoToSeekedDateResponseModel(requestModel.getSeekedDate());
-        return responseModel;
-    }
-}
