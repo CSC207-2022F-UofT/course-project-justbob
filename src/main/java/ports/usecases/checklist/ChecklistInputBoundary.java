3a8aafb980e73956e4f9f596f91b9a9545294e99
YuweiCai2003 <113796739+YuweiCai2003@users.noreply.github.com>
2022-12-06 18:28:33 -0500

    Delete ChecklistInputBoundary.java

--- src/main/java/ports/usecases/checklist/ChecklistInputBoundary.java
+++
@@ -1,7 +0,0 @@
-package ports.usecases.checklist;
-
-import ports.usecases.PathNotFoundError;
-
-public interface ChecklistInputBoundary {
-    ChecklistModel execute(String userName) throws PathNotFoundError;
-}
