65fa76f7d9c22995471643b5c8d482b42383d64c
YuweiCai2003 <113796739+YuweiCai2003@users.noreply.github.com>
2022-12-06 18:02:27 -0500

    Add files via upload

---
+++ src/main/java/entities/weightScheme/WeightScheme.java
@@ -0,0 +1,18 @@
+package entities.weightScheme;
+
+import java.util.List;
+
+public interface WeightScheme {
+    /**
+     * Compute the average mark earned with (marks) according to this weightScheme.
+     *
+     * Preconditions:
+     *      marks.length <= getNumberOfInstances()
+     *      for each mark in marks, 0 <= mark <= 100
+     * @param marks
+     * @return
+     */
+    public double computeWeighted(double[] marks);
+    public double getTotalWeight();
+    public int getNumberOfInstances();
+}
