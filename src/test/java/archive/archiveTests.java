a1ba4e35cd41a0e5f5dc3977a7bd98179b2435aa
AlanC12138 <alanchen0705@gmail.com>
2022-12-06 15:41:01 -0500

    simplified createResponse method for RemoveArchiveCourseUseCase
    added new test for removing archived course

--- src/test/java/archive/archiveTests.java
+++ src/test/java/archive/archiveTests.java
@@ -17,6 +17,7 @@ import ports.usecases.account.addSemesterCourse.AddSemesterCourseInputBoundary;
 import ports.usecases.account.archiveCourse.ArchiveCourseInputBoundary;
 import ports.usecases.account.archiveCourse.ArchiveCourseInputBoundary.CourseNotCompletedError;
 import usecases.account.ArchiveCourseUseCase;
+import usecases.account.RemoveArchiveCourseUseCase;
 
 import java.util.ArrayList;
 
@@ -41,5 +42,22 @@ public class archiveTests {
         entityGateway.saveAccount(account);
         ArchiveCourseUseCase myUseCase = new ArchiveCourseUseCase(entityGateway);
         myUseCase.execute("bob12345", "CSC207");
+    }
+
+    @Test
+    public void RemoveCourseTest(){
+        this.entityGateway = new InMemoryEntityGateway();
+        this.factory = new InMemoryEntityFactory();
+        Account account = factory.createAccount();
+        Archive archive = factory.createArchive();
+        Course course = factory.createCourse();
+        account.setArchive(archive);
+        account.setUsername("bob12345");
+        course.setCourseCode("CSC207");
+        archive.addCourse(course, "Fall 2022");
+        entityGateway.saveAccount(account);
+        RemoveArchiveCourseUseCase myUseCase = new RemoveArchiveCourseUseCase(entityGateway);
+        myUseCase.execute("bob12345", "CSC207");
+        Assertions.assertEquals(new ArrayList<>(), entityGateway.loadAccount("bob12345").getArchive().getCourses());
     }
 }
