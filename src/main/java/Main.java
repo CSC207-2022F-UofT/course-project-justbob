3af6c0e253caf8c361889f0dbc4c05e6e5e99a67
samielsabri <sami.elsabri@mail.utoronto.ca>
2022-12-07 13:20:43 -0500

    created temp application view as shortcut for UI "debugging"

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,11 @@ import inMemoryDB.InMemoryEntityFactory;
 import inMemoryDB.InMemoryEntityGateway;
 import ports.database.EntityFactory;
 import ports.database.EntityGateway;
+import ports.usecases.ApplicationResponse;
+import ports.usecases.account.addAccount.AddAccountRequest;
+import usecases.account.AddAccount.AddAccountController;
+import usecases.account.AddAccount.AddAccountUseCase;
+import views.ApplicationView;
 import views.LoginView;
 
 public class Main {
@@ -9,6 +14,16 @@ public class Main {
     private static final EntityFactory entityFactory = new InMemoryEntityFactory();
 
     public static void main(String[] args) {
-        new LoginView(entityGateway, entityFactory);
+        /*new LoginView(entityGateway, entityFactory);*/
+        AddAccountRequest accountRequest = new AddAccountRequest("Bob", "password1", "password1");
+        AddAccountUseCase usecase = new AddAccountUseCase(entityGateway, entityFactory);
+        usecase.execute(accountRequest);
+        ApplicationResponse tempResponse = new ApplicationResponse("Bob",
+                "Semester 1",
+                new String[]{},
+                new String[]{},
+                new Double[]{},
+                null);
+        new ApplicationView(entityGateway, entityFactory, tempResponse);
     }
 }
