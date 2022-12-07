f7fb68a6244880a495359d3af89e6bb086c80159
Billy <billyg3002@gmail.com>
2022-12-06 19:51:17 -0500

    GoToSeekedDate, RemoveStickyNote, and WriteToStickyNote use cases

---
+++ src/main/java/ports/usecases/calendar/removeStickyNoteUseCase/RemoveStickyNoteInteractor.java
@@ -0,0 +1,21 @@
+package ports.usecases.calendar.removeStickyNoteUseCase;
+
+import screens.RemoveStickyNoteDsGateway;
+
+public class RemoveStickyNoteInteractor implements RemoveStickyNoteInputBoundary {
+    private RemoveStickyNoteDsGateway dataAccessGateway;
+    private RemoveStickyNoteOutputBoundary removeStickyNoteremoveStickyNoteOutputBoundary;
+
+    public RemoveStickyNoteInteractor(RemoveStickyNoteDsGateway dataAccessGateway, RemoveStickyNoteOutputBoundary removeStickyNoteOutputBoundary) {
+        this.dataAccessGateway = dataAccessGateway;
+        this.removeStickyNoteremoveStickyNoteOutputBoundary = removeStickyNoteOutputBoundary;
+    }
+
+    @Override
+    public RemoveStickyNoteResponseModel execute(RemoveStickyNoteRequestModel requestModel) {
+        dataAccessGateway.delete(requestModel);
+        //File fileName = new File(System.getProperty("user.dir"), "src/main/java/inMemoryDB/StickyNotes/" + requestModel.getTitle() + ".txt");
+        //RemoveStickyNoteResponseModel responseModel = new RemoveStickyNoteResponseModel(fileName.exists());
+        return removeStickyNoteremoveStickyNoteOutputBoundary.prepareSuccessView(requestModel);
+    }
+}
