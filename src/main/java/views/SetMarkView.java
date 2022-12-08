207d89e0ae5cdb547c4c7ab169c7ac98b2758653
samielsabri <sami.elsabri@mail.utoronto.ca>
2022-12-07 22:34:48 -0500

    minor edits on SetMarkView

--- src/main/java/views/SetMarkView.java
+++ src/main/java/views/SetMarkView.java
@@ -17,7 +17,7 @@ public class SetMarkView {
 
         // JFrame class
         JFrame frame = new JFrame();
-        frame.setTitle("Set Mark of" + response.instanceName );
+        frame.setTitle("Set Mark of " + response.instanceName);
         frame.setLocation(new Point(500, 300));
         frame.add(panel);
         frame.setSize(new Dimension(400, 260));
@@ -49,6 +49,7 @@ public class SetMarkView {
             SetMarkRequest request = new SetMarkRequest(response.username, response.courseCode, response.assessmentTitle, response.instanceNumber, markValue);
             new SetMarkController(request, frame, entityGateway, entityFactory, parentFrame);
         });
+        frame.setVisible(true);
 
 
     }
