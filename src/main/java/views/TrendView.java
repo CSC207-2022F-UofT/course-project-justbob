c55f04a3ff0ac4fc73e65fd3edd953c2fd2c7608
Deep Jain <deepsjain123@gmail.com>
2022-12-08 17:00:34 -0500

    fixes

--- src/main/java/views/TrendView.java
+++ src/main/java/views/TrendView.java
@@ -22,11 +22,17 @@ public class TrendView {
         // frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         CategoryChart chart = new CategoryChart(1000, 500);
-        chart.addSeries(name, model.xData, model.yData);
+        if (model.yData.isEmpty()) {
+            // No Committed Data Label
+            JLabel label1 = new JLabel("No Committed Grades");
+            label1.setBounds(100, 8, 193, 20);
+            panel.add(label1);
+        } else {
+            chart.addSeries(name, model.xData, model.yData);
+            JPanel chartPanel = new XChartPanel<>(chart);
+            frame.add(chartPanel);
+        }
 
-        JPanel chartPanel = new XChartPanel<>(chart);
-
-        frame.add(chartPanel);
         frame.setVisible(true);
     }
 }
