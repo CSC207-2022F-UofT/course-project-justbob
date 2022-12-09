88a36a1a90d42b042231417850ce17ec6a0470c6
sunshine <armonjam@gmail.com>
2022-12-08 21:50:13 -0500

    Deleted Deprecated files

--- src/main/java/usecases/dataStorage_DEPRECATED/assessment/AssessmentDsGateway.java
+++
@@ -1,6 +0,0 @@
-package usecases.dataStorage_DEPRECATED.assessment;
-
-public interface AssessmentDsGateway {
-    boolean existsAssessment(AssessmentDsRequestModel requestModel);
-    AssessmentDsModel loadAssessment(AssessmentDsRequestModel requestModel) throws IllegalArgumentException;
-}
--- src/main/java/usecases/dataStorage_DEPRECATED/weightScheme/WeightSchemeDsModel.java
+++
@@ -1,5 +0,0 @@
-package usecases.dataStorage_DEPRECATED.weightScheme;
-
-import java.io.Serializable;
-
-public class WeightSchemeDsModel implements Serializable { }
