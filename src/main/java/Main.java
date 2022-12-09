5a2be60c86d0e7004a64e60691508b68afa548f5
sunshine <armonjam@gmail.com>
2022-12-08 22:52:08 -0500

    Added dummy account

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,7 @@ import views.LoginView;
 
 public class Main {
     private static final String DATABASE_FILE_PATH =
-            "src/main/java/database_file.json";
+            "src/main/java/database.ser";
 
     private static final EntityGateway entityGateway = new InMemoryEntityGateway(DATABASE_FILE_PATH);
     private static final EntityFactory entityFactory = new InMemoryEntityFactory();
